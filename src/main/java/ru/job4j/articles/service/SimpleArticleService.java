ea2b766a9fa7eaba2433ada7e19589750034bed2
PavelValger <pavelwalker@mail.ru>
2022-12-27 14:01:26 +0500

    2. GC. Out Of Mem. Тестовое задание. [#504790] изменил метод generate

--- src/main/java/ru/job4j/articles/service/SimpleArticleService.java
+++ src/main/java/ru/job4j/articles/service/SimpleArticleService.java
@@ -7,8 +7,7 @@ import ru.job4j.articles.model.Word;
 import ru.job4j.articles.service.generator.ArticleGenerator;
 import ru.job4j.articles.store.Store;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.stream.IntStream;
 
 public class SimpleArticleService implements ArticleService {
     private static final Logger LOGGER = LoggerFactory
@@ -22,15 +21,10 @@ public class SimpleArticleService implements ArticleService {
     @Override
     public void generate(Store<Word> wordStore, int count, Store<Article> articleStore) {
         LOGGER.info("Геренация статей в количестве {}", count);
-        List<Word> words = wordStore.findAll();
-        List<Article> articleList = new LinkedList<>();
-        for (int i = 0; i < count; i++) {
-            LOGGER.info("Сгенерирована статья № {}", i);
-            articleList.add(articleGenerator.generate(words));
-            if (articleList.size() == 10000) {
-                articleList.forEach(articleStore::save);
-                articleList.clear();
-            }
-        }
+        var words = wordStore.findAll();
+        IntStream.iterate(0, i -> i < count, i -> i + 1)
+                .peek(i -> LOGGER.info("Сгенерирована статья № {}", i))
+                .mapToObj((x) -> articleGenerator.generate(words))
+                .forEach(articleStore::save);
     }
 }
